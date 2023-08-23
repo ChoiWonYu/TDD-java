cf68cba94900af9db31b2f8a2d1c9177b117c65a
ChoiWonYu <phillip07290@naver.com>
2023-08-23 20:23:34 +0900

    refactor: 충족한 개수를 이용해 리팩토링

--- src/main/java/chap02/PasswordStrengthMeter.java
+++ src/main/java/chap02/PasswordStrengthMeter.java
@@ -4,17 +4,14 @@ public class PasswordStrengthMeter {
     public PasswordStrength meter(String password) {
         if(password==null||password.isEmpty())return PasswordStrength.INVALID;
 
-        boolean isLengthEnough=password.length()>=8;
-        boolean isContainsNum=isContainsNumber(password);
-        boolean isContainsUpp=isContainsUpperCase(password);
+        int metCounts=0;
 
-        if(isLengthEnough&&!isContainsNum&&!isContainsUpp)return PasswordStrength.WEAK;
-        if(isContainsNum&&!isLengthEnough&&!isContainsUpp)return PasswordStrength.WEAK;
-        if(isContainsUpp&&!isContainsNum&&!isLengthEnough)return PasswordStrength.WEAK;
+        if(password.length()>=8)metCounts++;
+        if(isContainsNumber(password))metCounts++;
+        if(isContainsUpperCase(password))metCounts++;
 
-        if(!isLengthEnough)return PasswordStrength.NORMAL;
-        if(!isContainsNumber(password))return PasswordStrength.NORMAL;
-        if(!isContainsUpperCase(password))return PasswordStrength.NORMAL;
+        if(metCounts==1)return PasswordStrength.WEAK;
+        if(metCounts==2)return PasswordStrength.NORMAL;
         return PasswordStrength.STRONG;
     }
 
