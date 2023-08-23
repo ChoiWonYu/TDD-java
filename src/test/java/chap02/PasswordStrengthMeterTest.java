e1bfb67746686d1f404df81959b2898a2ef3e3d0
ChoiWonYu <phillip07290@naver.com>
2023-08-23 18:58:43 +0900

    refactor: PasswordStrengthMeter 클래스 멤버로 분리

--- src/test/java/chap02/PasswordStrengthMeterTest.java
+++ src/test/java/chap02/PasswordStrengthMeterTest.java
@@ -1,18 +1,18 @@
 package chap02;
 
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class PasswordStrengthMeterTest {
+    private PasswordStrengthMeter meter=new PasswordStrengthMeter();
+
     @Test
     @DisplayName("모든 규칙을 충족하는 경우")
     public void meetsAllCriteria_Then_String() throws Exception {
         //given
         String password = "ab12!@AB";
-        PasswordStrengthMeter meter = new PasswordStrengthMeter();
         //when
         PasswordStrength result = meter.meter(password);
         //then
@@ -23,7 +23,6 @@ public class PasswordStrengthMeterTest {
     @DisplayName("길이만 8글자 미만이고 나머지 조건은 충족하는 경우")
     public void meetsOtherCritia_except_for_Length_then_Normal() throws Exception {
         //given
-        PasswordStrengthMeter meter = new PasswordStrengthMeter();
         String password = "ab12!@A";
         //when
         PasswordStrength result = meter.meter(password);
@@ -35,7 +34,6 @@ public class PasswordStrengthMeterTest {
     @DisplayName("숫자를 포함하지 않고 나머지 조건은 충족하는 경우")
     public void meetsOtherCritia_except_for_number_then_Normal() throws Exception {
         //given
-        PasswordStrengthMeter meter = new PasswordStrengthMeter();
         String password = "ab!@ABqwer";
         //when
         PasswordStrength result = meter.meter(password);
