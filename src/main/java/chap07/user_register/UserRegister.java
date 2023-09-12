c5b864ad330c6b0577c25ba609d0d2708e12f25e
WonyuChoi <phillip07290@naver.com>
2023-09-12 11:41:52 +0900

    feat: add user repository to constructor

--- src/main/java/chap07/user_register/UserRegister.java
+++ src/main/java/chap07/user_register/UserRegister.java
@@ -1,14 +1,18 @@
 package chap07.user_register;
 
 import chap07.user_register.exception.WeakPasswordException;
+import chap07.user_register.type.UserRepository;
 import chap07.user_register.type.WeakPasswordChecker;
 
 public class UserRegister {
 
     private WeakPasswordChecker passwordChecker;
 
-    public UserRegister(WeakPasswordChecker passwordChecker) {
+    private UserRepository userRepository;
+
+    public UserRegister(WeakPasswordChecker passwordChecker,UserRepository userRepository) {
         this.passwordChecker=passwordChecker;
+        this.userRepository=userRepository;
     }
 
     public void register(String id, String pw, String email) {
